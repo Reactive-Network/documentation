bea9a1f57c17888057665a1977c30a423d81ae40
Konstantin Koniukov <konstantin@parsiq.net>
2024-08-19 17:59:19 +0700

    minor edits

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
         },
         docs: {
           routeBasePath: "/",
-          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarPath: "./sidebars.js",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/Reactive-Network/documentation/edit/main/",
--- src/theme/EditThisPage/index.js
+++ src/theme/EditThisPage/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 export default function EditThisPage({ editUrl }) {
     return (
         <a href={editUrl} target="_blank" rel="noopener noreferrer">
-            Report an issue
+            Edit this page
         </a>
     );
 }
